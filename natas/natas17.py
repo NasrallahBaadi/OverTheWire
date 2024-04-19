9640e6a1c63fe3d8a366ba8dbda8ec2279b36e1c
Nasrallah <33826467+NasrallahBaadi@users.noreply.github.com>
2024-04-19 11:13:54 +0100

    Update natas17.py

--- natas/natas17.py
+++ natas/natas17.py
@@ -41,7 +41,7 @@ def sqli(url, user, password, session, proxies=None):
 
 def main():
     # Parse command-line arguments
-    parser = argparse.ArgumentParser(description="Grabs the password of natas18 by exploiting a blind sql injection.")
+    parser = argparse.ArgumentParser(description="Grabs the password of natas18 by exploiting a time-based sql injection.")
     parser.add_argument('-p', '--password', help='Password of Natas17.', required=True, metavar='Password')
     parser.add_argument('-x', '--proxy', help='Sends requests through Burp Suite proxy at 127.0.0.1:8080.', action='store_true')
     args = parser.parse_args()
